e4a896878d8eda1a81c42d4f31d093be6cd8deba
Sawin3048 <samuelalagre022@gmail.com>
2024-07-15 23:04:51 -0400

    create pesistance implementation with prisma orm
    rebase

---
+++ src/Contexts/Items/infraestructure/persistance/PrismaItemRepository.ts
@@ -0,0 +1,36 @@
+import { ItemRepository } from '../../domain/ItemRepository'
+import { prisma } from '../../../Shared/infraestructure/persistence/prisma/db'
+import { Item } from '../../domain/Item'
+
+export class PrismaItemRepository implements ItemRepository {
+  async save(item: Item) {
+    await prisma.item.create({
+      data: {
+        code: item.code.toString(),
+        description: item.description.toString(),
+        saleType: item.saleType.toString(),
+        basePrice: item.price.basePrice.value,
+        unitaryPrice: item.price.unitaryPrice.value,
+        wholesalePrice: item.price.wholesalePrice.value,
+        stock: item.stock.value
+      }
+    })
+  }
+
+  async searchAll() {
+    const rawData = await prisma.item.findMany()
+    return rawData.map(data => {
+      return Item.fromPrimitives({
+        code: Number(data.code),
+        description: data.code,
+        saleType: data.saleType,
+        price: {
+          basePrice: data.basePrice,
+          unitaryPrice: data.unitaryPrice,
+          wholesalePrice: data.wholesalePrice
+        },
+        stock: data.stock
+      })
+    })
+  }
+}
