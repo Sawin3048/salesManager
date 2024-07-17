95de75fcd488368958ade7f17bac58f175b6fb0d
Sawin3048 <samuelalagre022@gmail.com>
2024-07-16 23:45:52 -0400

    Add itemCreator test case, when one property is undefined

--- tests/Contexts/Items/aplication/ItemCreator.test.ts
+++ tests/Contexts/Items/aplication/ItemCreator.test.ts
@@ -1,4 +1,5 @@
 import { ItemCreator } from '../../../../src/Contexts/Items/application/Create/ItemCreator'
+import { InvalidArgumentError } from '../../../../src/Contexts/Shared/domain/InvalidArgumentError'
 import { ItemRepositoryMock } from '../__mocks__/ItemRepositoryMock'
 import { ItemMother } from '../domain/ItemMother'
 
@@ -19,5 +20,34 @@ describe('ItemCreator', () => {
     )
 
     repository.assertSaveHasBeenCalledWith(item)
+  })
+
+  it('Should throw an error when trying to create an item without one property', async () => {
+    const repository = new ItemRepositoryMock()
+    const service = new ItemCreator(repository)
+
+    const undefinedPropiety = undefined as any
+
+    const serviceRunner = async (index: number) => {
+      const item = ItemMother.random()
+      const primitives = item.toPrimitives()
+
+      await service.run(
+        index === 1 ? primitives.code : undefinedPropiety,
+        index === 2 ? primitives.description : undefinedPropiety,
+        index === 3 ? primitives.saleType : undefinedPropiety,
+        {
+          basePrice: index === 4 ? primitives.price.basePrice : undefinedPropiety,
+          unitaryPrice: index === 5 ? primitives.price.unitaryPrice : undefinedPropiety,
+          wholesalePrice: index === 6 ? primitives.price.wholesalePrice : undefinedPropiety
+        },
+        index === 7 ? primitives.stock : undefinedPropiety
+      )
+    }
+
+    expect.assertions(7)
+    for (let index = 1; index <= 7; index++) {
+      await expect(serviceRunner(index)).rejects.toThrow(InvalidArgumentError)
+    }
   })
 })
