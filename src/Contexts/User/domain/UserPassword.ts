4dce8d4025658e147301679adaea082d2b7a925e
Sawin3048 <samuelalagre022@gmail.com>
2024-07-19 14:20:30 -0400

    add return type on UserPassword for correct typing

--- src/Contexts/User/domain/UserPassword.ts
+++ src/Contexts/User/domain/UserPassword.ts
@@ -9,7 +9,7 @@ export class UserPassword {
     this.value = this.ensureIsValid(password)
   }
 
-  private ensureIsValid(value: string) {
+  private ensureIsValid(value: string): string {
     const shema = Joi.string().exist().min(8)
     return validateShema({ value, shema, propertyName: this.name })
   }
