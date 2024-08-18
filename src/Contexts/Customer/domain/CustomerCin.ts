7d691b4ac56d2f9044c1c5272edcabd4e9ca52a3
Samuel3048 <samuelalegre022@gmail.com>
2024-08-17 22:36:47 -0400

    Algoritmo verificador de RUC

--- src/Contexts/Customer/domain/CustomerCin.ts
+++ src/Contexts/Customer/domain/CustomerCin.ts
@@ -13,4 +13,41 @@ export class CustomerRUC {
     const shema = Joi.string().exist().min(1)
     return validateShema({ shema, value, propertyName: this.name })
   }
+
+  private verifyRUC(ruc: string, baseMax = 11) {
+    let rucTransformado = ''
+    let sumaTotal = 0
+    let multiplicador = 2
+
+    // Convertir caracteres no numéricos a su valor ASCII
+    for (let i = 0; i < ruc.length; i++) {
+      const caracter = ruc[i].toUpperCase()
+      const codigoAscii = caracter.charCodeAt(0)
+
+      if (codigoAscii >= 48 && codigoAscii <= 57) {
+        // Si es un número (0-9)
+        rucTransformado += caracter
+      } else {
+        // Si es una letra u otro carácter lo convierte el su valor numerico en Ascii
+        // ej:'A' se convertira en 65 y 'B' en 66
+        rucTransformado += codigoAscii.toString()
+      }
+    }
+
+    // Calcular la suma total utilizando el algoritmo
+    for (let i = rucTransformado.length - 1; i >= 0; i--) {
+      if (multiplicador > baseMax) {
+        multiplicador = 2
+      }
+      const numeroActual = parseInt(rucTransformado[i], 10)
+      sumaTotal += numeroActual * multiplicador
+      multiplicador++
+    }
+
+    // Obtener el dígito verificador
+    const resto = sumaTotal % 11
+    const digitoVerificador = resto > 1 ? 11 - resto : 0
+
+    return digitoVerificador
+  }
 }
--- src/Contexts/Receipt/domain/ReceiptCreatedAt.ts
+++ src/Contexts/Receipt/domain/ReceiptCreatedAt.ts
@@ -1,13 +1,20 @@
 import Joi from 'joi'
-import { ValueObject } from '../../Shared/domain/value-object/ValueObject'
+import { validateShema } from '../../Shared/infraestructure/JoiShemaValidate'
 
 export class ReceiptCreatedAt {
   private readonly name = 'createdAt'
-
-  constructor(createdAt: string) {
+  readonly value
+  constructor(createdAt: string | Date) {
+    this.value = this.ensureIsValid(createdAt)
   }
 
-  private ensureIsValid(value: string): Date {
+  private ensureIsValid(value: string | Date): Date {
+    let date = value
+    if (typeof value === 'string') {
+      date = new Date(value)
+    }
     const shema = Joi.date().exist()
+
+    return validateShema({ shema, value: date, propertyName: this.name })
   }
 }
